64765cd8ea96bded80aac4e02cf32b818830b2fb
Matheus Aguiar <matheus.pit@gmail.com>
2025-05-18 13:00:17 -0300

    Add and update tests

--- test/libsolidity/syntaxTests/storageLayoutSpecifier/constant_from_module.sol
+++ test/libsolidity/syntaxTests/storageLayoutSpecifier/constant_from_module.sol
@@ -5,4 +5,3 @@ uint constant x = 77;
 import "A" as M;
 contract C layout at M.x{ }
 // ----
-// TypeError 1505: (B:38-41): The base slot expression contains elements that are not yet supported by the internal constant evaluator and therefore cannot be evaluated at compilation time.
